50612ca428dcbad1f672b7b1dc14479de72d3c3f
tiger.guo <43375138@qq.com>
2015-10-12 17:42:47 +0800

    邮箱注册

--- sms/sms.js
+++ sms/sms.js
@@ -2,10 +2,12 @@
 		  var mobile = document.getElementById(mobile_field).value;
 		  Ajax.call('sms/sms.php?act=send', 'mobile=' + mobile, sendSmsResponse, 'POST', 'JSON');
 		}
+		
 		function sendSmsResponse(result){
 		  if (result.code==2){
 			    RemainTime();		
 				alert('手机验证码已经成功发送到您的手机');
+				//insert verify code into data into database;
 		  }else{
 			  if(result.msg){
 				alert(result.msg);
@@ -16,7 +18,12 @@
 		}
 		function register2(){
 			//add by wanggz
-			//return register();
+//			return register();
+			var reg_type = document.getElementById('reg_type').value;
+			if(reg_type=='1'){
+				return register_by_email();
+			}
+			
 			var mobile = document.getElementById(mobile_field).value;
 			if (mobile_field != ''){
 			  var mobile_code = document.getElementById("mobile_code").value;
@@ -30,6 +37,23 @@
 			}
 		  return register();			
 		}
+		
+		function register_by_email(){
+			//add by tiger.guo 20151012
+			var email = document.getElementById('email').value;
+			if (email != ''){
+			 /*var result = Ajax.call('sms/sms.php?act=check', 'mobile=' + mobile + '&mobile_code=' + mobile_code, null, 'POST', 'JSON', false);	
+			 if (result.code==2){
+					return register();
+			  }else{
+					alert(result.msg);
+					return false;
+			  }*/
+			return register();		
+			}
+		  return register();			
+		}
+		
 		function validateMobileCode(){
 			var mobile = document.getElementById(mobile_field).value;
 			if (mobile_field != ''){

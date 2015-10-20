46776acdce52efe940e134b87ad37b1699039aea
tiger.guo <43375138@qq.com>
2015-10-20 18:30:07 +0800

    用户中心-欢迎页

--- data/database/his_old.sql
+++ data/database/his_old.sql
@@ -15,3 +15,23 @@ alter table ecs_goods_attr add attr_product_sn varchar(60) COMMENT '货号' ;
 alter table ecs_goods_attr add attr_product_number SMALLINT(5) COMMENT '库存'; 
 
 
+CREATE TABLE `ecs_goods_attr_sku` (
+  `sku_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键编号',
+  `attr_price` decimal(10,2) COMMENT '价格',
+  `stock_quantity` decimal(10,2) COMMENT '库存',
+  `goods_sn` varchar(60) COMMENT '货号',
+  `warsehouse_id` int(10) COMMENT '仓库',
+  PRIMARY KEY (`sku_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=476 DEFAULT CHARSET=utf8 COMMENT='商品sku信息表';
+
+
+CREATE TABLE `ecs_goods_attr_sku_detail` (
+  `sku_detail_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键编号',
+  `sku_id` int(10) unsigned NOT NULL COMMENT '关联sku编号',
+  `goods_attr_id` int(10) unsigned NOT NULL COMMENT '商品属性编号',  
+  PRIMARY KEY (`sku_detail_id`),
+ CONSTRAINT `fk_sku_detail_detail_ref_sku` FOREIGN KEY (`sku_id`) REFERENCES `ecs_goods_attr_sku` (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=476 DEFAULT CHARSET=utf8 COMMENT='商品sku明细表';
+
+alter table ecs_users add last_sign_time int(10) COMMENT '最后一次签到时间';
+
